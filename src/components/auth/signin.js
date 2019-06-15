25787ff165ca87dabd35236aa3b010c000a9cf0a
ShakeriaCodes <yogi_keri@icloud.com>
2019-06-15 08:24:38 -0700

    styled formfields components

--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 
 import SignInForm from './signinForm';
+import PageTitle from '../pageTitle';
 
 class SignIn extends Component {
+    
     onSubmit = (fields) => {
         console.log(fields);
     }
@@ -10,6 +12,7 @@ class SignIn extends Component {
     render() {
         return (
             <div className='sign-in'>
+                <PageTitle className='sign-in__page-title' title='Login' />
                 <SignInForm onSubmit={this.onSubmit} className='sign-in__form' />
             </div>
         )
--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-
+import { reduxForm, Field } from 'redux-form';
 import { FormInput, FormButton } from '../formFields';
-
 class SignInForm extends Component {
     render() {
         const { className, handleSubmit } = this.props;
@@ -13,7 +12,14 @@ class SignInForm extends Component {
                 placeholder='Email'
                 name='email'
                 component={FormInput}/>
-                  <Field className='sign-in-form__login'
+                <Field className='sign-in-form__password'
+                type='password'
+                title='Password'
+                placeholder='Password'
+                name='password'
+                component={FormInput}/>
+                <div className='sign-in-form__line'></div>
+                <Field className='sign-in-form__login'
                 onClick={() => console.log('tryna submit')}
                 type='submit'
                 title='Login'
@@ -23,9 +29,7 @@ class SignInForm extends Component {
         )
     }
 }
-
 SignInForm = reduxForm({
     form: 'SignInForm'
 })(SignInForm);
-
 export default SignInForm;
