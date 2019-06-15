7c9583ef74ac301eae0cc3be8c7f9987d54458c5
ShakeriaCodes <yogi_keri@icloud.com>
2019-06-15 11:48:17 -0700

    built headernavbar reducer & sign in

--- src/components/auth/signinForm.js
+++ src/components/auth/signinForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import { reduxForm, Field } from 'redux-form';
 
 import { FormInput, FormButton } from '../formFields';
@@ -42,7 +43,7 @@ class SignInForm extends Component {
                 component={FormInput}/>
                 <div className='sign-in-form__line'></div>
                 <Field className='sign-in-form__login'
-                onClick={() => console.log('tryna submit')}
+                onClick={() => history.push('/account')}
                 type='submit'
                 title='Login'
                 name='login'
@@ -52,7 +53,9 @@ class SignInForm extends Component {
         )
     }
 }
+
 SignInForm = reduxForm({
     form: 'SignInForm'
 })(SignInForm);
+
 export default SignInForm;
