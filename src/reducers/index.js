0ab2aa99fdc178053cbdd988903fded7049c94cf
ShakeriaCodes <yogi_keri@icloud.com>
2019-06-16 19:59:01 -0700

    built user reducer with fetch purchases action creator to fetch user purchases data

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import { reducer as form } from 'redux-form'; 
 
 import headerNavbar from './headernavbarReducer';
+import user from './userReducer';
 
 const rootReducer = combineReducers({
   form,
-  headerNavbar
+  headerNavbar,
+  user
 });
 
 export default rootReducer;
