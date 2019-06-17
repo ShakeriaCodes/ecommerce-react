72a2c5e8f97731667cf0c7e657723066140fd3bc
ShakeriaCodes <yogi_keri@icloud.com>
2019-06-16 20:38:05 -0700

    finished purchases component

--- src/components/account/purchaseHistory.js
+++ src/components/account/purchaseHistory.js
@@ -9,8 +9,10 @@ class PurchaseHistory extends Component {
         return (
             <div className='purchase-history'>
                 <PageTitle className='purchase-history__page-title' title='Purchase History'/>
-                <Purchases className='purchase-history__purchases'/>
-                <PurchaseDetail className='purchase-history__detail'/>
+                <div className='purchase-history__content'>
+                    <Purchases className='purchase-history__purchases'/>
+                    <PurchaseDetail className='purchase-history__detail'/>
+                </div>
                 <div className='purchase-history__border-bottom'></div>
 
             </div>
--- src/components/account/purchases.js
+++ src/components/account/purchases.js
@@ -16,7 +16,11 @@ class Purchases extends Component {
             <div className={`${className} purchases`}>
                 {
                     this.props.purchases.map(purchase => {
-                        return <div>{purchase.title}</div>
+                        return (
+                            <div key={purchase._id} className='purchases__purchase purchase'>
+                                <img className='purchase__img' src='http://via.placeholder.com/80x80'/>
+                            </div>
+                        )
                     })
                 }
             </div>
